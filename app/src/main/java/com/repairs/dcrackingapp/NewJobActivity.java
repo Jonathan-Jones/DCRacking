ef89f02e2c28b9fbde02ecb6cc5c2f40f5d1f416
Jonathan-Jones <jonathan.jones84@gmail.com>
2014-11-13 00:15:23 -0500

    Adding controls for the new job activity

--- app/src/main/java/com/repairs/dcrackingapp/NewJobActivity.java
+++ app/src/main/java/com/repairs/dcrackingapp/NewJobActivity.java
@@ -3,17 +3,28 @@ package com.repairs.dcrackingapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Jonathan on 10/6/2014.
  */
 public class NewJobActivity extends Activity
 {
+
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_job);
+
+
+        EditText editText = (EditText) findViewById( R.id.new_job_date_edit_text );
+        SimpleDateFormat sdf = new SimpleDateFormat( "yyyy/MM/dd" );
+        editText.setText(sdf.format(new Date()));
     }
 
     @Override
@@ -23,10 +34,6 @@ public class NewJobActivity extends Activity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 }
